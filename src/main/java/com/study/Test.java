242888540a3b0ada4183a3583f25a9a5a0002371
mack <liangling627@qq.com>
2018-11-15 13:16:10 +0800

    第2次提交

--- src/main/java/com/study/Test.java
+++ src/main/java/com/study/Test.java
@@ -21,6 +21,8 @@ public class Test {
 		for (int i = 0; i < 100; i++) {
 			System.out.println(ThreadLocalRandom.current().nextInt(10));
 			System.out.println("第一次提交");
+			System.out.println("第2次提交");
+
 
 		}
 
